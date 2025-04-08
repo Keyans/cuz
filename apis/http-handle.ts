1f178e04b6a9fc9d1a5c7732e87585a6b49e8d97
吴良玉 <wuliangyu@hosonsoft.com>
2025-04-08 22:02:56 +0800

    feat: 测试问题

--- apis/http-handle.ts
+++ apis/http-handle.ts
@@ -2,7 +2,7 @@ import request from "./axiosConfig";
 import type { RequestData, R } from "./http.type";
 import type { AxiosRequestConfig, AxiosResponse } from "axios";
 import { TOKEN_OVERDUE } from "./http.type";
-import debounce from "lodash/debounce";
+import { debounce } from "lodash";
 
 abstract class AbstractHttp {
   protected abstract doRequest(
